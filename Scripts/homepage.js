ce4e9a2701de9a8f84ebf2fabb49e808d0d419bc
Crovvyboi <81975137+Crovvyboi@users.noreply.github.com>
2024-05-07 15:53:00 +0200

    chart update

--- Scripts/homepage.js
+++ Scripts/homepage.js
@@ -18,4 +18,95 @@ function inUitklappenZonnepanelen() {
         // Kan geen verbinding maken met de aaadlander
         document.getElementById("statusZonnepaneel").innerHTML = "Kan niet verbinden"
     }
+
+
+    // Tijdelijk, wordt later geactiveerd via de websocket
+    popStroomGraph();
+    popSpanningGraph();
+}
+
+function popStroomGraph() {
+    // Tijdelijke array, wordt vervangen door websocket call
+    const stroomArray = [15, 6, 9, 4, 1, 7, 19, 11, 5, 18, 16, 14, 17, 20, 13];
+
+    var tbodyRef = document.getElementById('stroomgrafiek').getElementsByTagName('tbody')[0];
+    var newbody = document.createElement('tbody');
+
+    var maxValue = findMax(stroomArray);
+    
+    // Plaats elke entry in de grafiek
+    for (let index = 0; index < stroomArray.length; index++) {
+        const element = stroomArray[index];
+        
+        if (index == 0) {
+            var newrow = newbody.insertRow();
+            var newcell = newrow.insertCell();
+            newcell.style = "--end: " + calculateDecimal(element, maxValue) + ";"
+    
+            var newtext = document.createTextNode(element + " mA");
+            newcell.appendChild(newtext);
+        }
+        else{
+            var newrow = newbody.insertRow();
+            var newcell = newrow.insertCell();
+            newcell.style = "--start: " + calculateDecimal(stroomArray[index - 1], maxValue) + "; --end: " + calculateDecimal(element, maxValue) + ";"
+    
+            var newtext = document.createTextNode(element + " mA");
+            newcell.appendChild(newtext);
+        }
+    }
+
+    tbodyRef.parentNode.replaceChild(newbody, tbodyRef);
+}
+
+function popSpanningGraph() {
+    // Tijdelijke array, wordt vervangen door websocket call
+    const spanningArray = [17, 1, 10, 14, 4, 16, 11, 20, 12, 13, 19, 8, 6, 2, 3];
+
+    var tbodyRef = document.getElementById('spanninggrafiek').getElementsByTagName('tbody')[0];
+    var newbody = document.createElement('tbody');
+
+    var maxValue = findMax(spanningArray);
+    
+    // Plaats elke entry in de grafiek
+    for (let index = 0; index < spanningArray.length; index++) {
+        const element = spanningArray[index];
+        
+        if (index == 0) {
+            var newrow = newbody.insertRow();
+            var newcell = newrow.insertCell();
+            newcell.style = "--end: " + calculateDecimal(element, maxValue) + ";"
+    
+            var newtext = document.createTextNode(element + " V");
+            newcell.appendChild(newtext);
+        }
+        else{
+            var newrow = newbody.insertRow();
+            var newcell = newrow.insertCell();
+            newcell.style = "--start: " + calculateDecimal(spanningArray[index - 1], maxValue) + "; --end: " + calculateDecimal(element, maxValue) + ";"
+    
+            var newtext = document.createTextNode(element + " V");
+            newcell.appendChild(newtext);
+        }
+    }
+
+    tbodyRef.parentNode.replaceChild(newbody, tbodyRef);
+}
+
+function findMax(array){
+    var maxValue = 0;
+
+    array.forEach(element => {
+        if (element > maxValue) {
+            maxValue = element;
+        }
+    });
+    
+    return maxValue;
+}
+
+function calculateDecimal(number, maxValue){
+    var decimalvalue = ((number * 100) / maxValue) / 100;
+
+    return decimalvalue;
 }
